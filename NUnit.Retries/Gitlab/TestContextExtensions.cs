e55b4dbcf3a2541d01d792c1e580b327372a15fc
politov.v <politov.v@skbkontur.ru>
2025-07-11 16:08:25 +0500

    Fix

--- NUnit.Retries/Gitlab/TestContextExtensions.cs
+++ NUnit.Retries/Gitlab/TestContextExtensions.cs
@@ -5,6 +5,7 @@ using System.IO;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using NUnit.Framework.Internal;
+using SkbKontur.NUnit.Retries.TeamCity;
 
 namespace SkbKontur.NUnit.Retries.Gitlab
 {
