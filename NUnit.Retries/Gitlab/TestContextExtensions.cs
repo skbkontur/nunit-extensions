70c7520d44b02e4e3695643580e96dd9ee287b32
politov.v <politov.v@skbkontur.ru>
2025-07-11 15:50:21 +0500

    Test

--- NUnit.Retries/Gitlab/TestContextExtensions.cs
+++ NUnit.Retries/Gitlab/TestContextExtensions.cs
@@ -28,26 +28,26 @@ namespace SkbKontur.NUnit.Retries.Gitlab
         private static void TestStarted(this TextWriter writer, IDictionary<string, string> props, DateTimeOffset start)
         {
             writer.WriteLine(ServiceMessageFormatter.FormatMessage("testStarted", new Dictionary<string, string>(props)
-                {
-                    ["timestamp"] = $"{start:yyyy-MM-dd'T'HH:mm:ss.fff}+0000",
-                }));
+            {
+                ["timestamp"] = $"{start:yyyy-MM-dd'T'HH:mm:ss.fff}+0000",
+            }));
         }
 
         private static void TestStdOut(this TextWriter writer, IDictionary<string, string> props, string output, int attempt, int tryCount)
         {
             writer.WriteLine(ServiceMessageFormatter.FormatMessage("testStdOut", new Dictionary<string, string>(props)
-                {
-                    ["timestamp"] = $"{DateTimeOffset.UtcNow:yyyy-MM-dd'T'HH:mm:ss.fff}+0000",
-                    ["out"] = $"Test failed on attempt {attempt}/{tryCount}, will retry\n\n" + output,
-                }));
+            {
+                ["timestamp"] = $"{DateTimeOffset.UtcNow:yyyy-MM-dd'T'HH:mm:ss.fff}+0000",
+                ["out"] = $"Test failed on attempt {attempt}/{tryCount}, will retry\n\n" + output,
+            }));
         }
 
         private static void TestFailed(this TextWriter writer, IDictionary<string, string> props, string? message, string? stackTrace)
         {
             var testFailedProps = new Dictionary<string, string>(props)
-                {
-                    ["timestamp"] = $"{DateTimeOffset.UtcNow:yyyy-MM-dd'T'HH:mm:ss.fff}+0000",
-                };
+            {
+                ["timestamp"] = $"{DateTimeOffset.UtcNow:yyyy-MM-dd'T'HH:mm:ss.fff}+0000",
+            };
 
             if (message != null)
             {
@@ -65,9 +65,9 @@ namespace SkbKontur.NUnit.Retries.Gitlab
         private static void TestFinished(this TextWriter writer, IDictionary<string, string> props)
         {
             writer.WriteLine(ServiceMessageFormatter.FormatMessage("testFinished", new Dictionary<string, string>(props)
-                {
-                    ["timestamp"] = $"{DateTimeOffset.UtcNow:yyyy-MM-dd'T'HH:mm:ss.fff}+0000",
-                }));
+            {
+                ["timestamp"] = $"{DateTimeOffset.UtcNow:yyyy-MM-dd'T'HH:mm:ss.fff}+0000",
+            }));
         }
 
         private static Dictionary<string, string> GetTestProperties(ITest test)
@@ -76,16 +76,16 @@ namespace SkbKontur.NUnit.Retries.Gitlab
             var suiteName = assembly.GetName().Name!;
 
             return new Dictionary<string, string>
-                {
-                    ["name"] = $"{suiteName}: {test.FullName}",
-                    ["captureStandardOutput"] = "false",
-                    ["suiteName"] = suiteName,
-                    ["testSource"] = assembly.Location,
-                    ["displayName"] = test.Name,
-                    ["fullyQualifiedName"] = test.FullName,
-                    ["id"] = Guid.NewGuid().ToString(),
-                    ["flowId"] = Guid.NewGuid().ToString(),
-                };
+            {
+                ["name"] = $"{suiteName}: {test.FullName}",
+                ["captureStandardOutput"] = "false",
+                ["suiteName"] = suiteName,
+                ["testSource"] = assembly.Location,
+                ["displayName"] = test.Name,
+                ["fullyQualifiedName"] = test.FullName,
+                ["id"] = Guid.NewGuid().ToString(),
+                ["flowId"] = Guid.NewGuid().ToString(),
+            };
         }
     }
 }
