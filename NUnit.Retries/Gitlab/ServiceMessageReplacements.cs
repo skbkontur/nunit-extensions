f1774699cf0e1f38506d4ecf13a5befccdd8dbaf
politov.v <politov.v@skbkontur.ru>
2025-07-11 15:56:23 +0500

    Fix

--- NUnit.Retries/Gitlab/ServiceMessageReplacements.cs
+++ NUnit.Retries/Gitlab/ServiceMessageReplacements.cs
@@ -15,44 +15,44 @@ namespace SkbKontur.NUnit.Retries.Gitlab
             {
                 switch (ch)
                 {
-                case '|':
-                    sb.Append("||");
-                    break; //
-                case '\'':
-                    sb.Append("|'");
-                    break; //
-                case '\n':
-                    sb.Append("|n");
-                    break; //
-                case '\r':
-                    sb.Append("|r");
-                    break; //
-                case '[':
-                    sb.Append("|[");
-                    break; //
-                case ']':
-                    sb.Append("|]");
-                    break; //
-                case '\u0085':
-                    sb.Append("|x");
-                    break; //\u0085 (next line)=>|x
-                case '\u2028':
-                    sb.Append("|l");
-                    break; //\u2028 (line separator)=>|l
-                case '\u2029':
-                    sb.Append("|p");
-                    break; //
-                default:
-                    if (ch > 127)
-                    {
-                        sb.Append($"|0x{(ulong)ch:x4}");
-                    }
-                    else
-                    {
-                        sb.Append(ch);
-                    }
+                    case '|':
+                        sb.Append("||");
+                        break; //
+                    case '\'':
+                        sb.Append("|'");
+                        break; //
+                    case '\n':
+                        sb.Append("|n");
+                        break; //
+                    case '\r':
+                        sb.Append("|r");
+                        break; //
+                    case '[':
+                        sb.Append("|[");
+                        break; //
+                    case ']':
+                        sb.Append("|]");
+                        break; //
+                    case '\u0085':
+                        sb.Append("|x");
+                        break; //\u0085 (next line)=>|x
+                    case '\u2028':
+                        sb.Append("|l");
+                        break; //\u2028 (line separator)=>|l
+                    case '\u2029':
+                        sb.Append("|p");
+                        break; //
+                    default:
+                        if (ch > 127)
+                        {
+                            sb.Append($"|0x{(ulong)ch:x4}");
+                        }
+                        else
+                        {
+                            sb.Append(ch);
+                        }
 
-                    break;
+                        break;
                 }
             }
 
